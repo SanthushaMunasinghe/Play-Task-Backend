77364084ba181e4b1d4eb71589619c88645a3e29
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2023-03-09 13:14:08 +0530

    create-classroom

--- api/Classroom/create-classroom.js
+++ api/Classroom/create-classroom.js
@@ -1,17 +1,28 @@
 const Classroom = require("../../models/Classroom");
 
+const Grade = require("../../models/Grade");
+
 async function createClassroom(req, res) {
   try {
-    const existingClass = await Classroom.findOne({ name: req.body.name });
-    if (existingClass) {
-      res.status(400).json({ message: "Classroom already exists" });
-    } else {
-      const classroom = new Classroom({
-        name: req.body.name,
-        grade: req.body.grade,
+    const existingGrade = await Grade.findOne({
+      $and: [{ number: req.body.grade }, { institution: req.body.institution }],
+    });
+    if (existingGrade) {
+      const existingClass = await Classroom.findOne({
+        $and: [{ name: req.body.name }, { grade: existingGrade._id }],
       });
-      const newClass = await classroom.save();
-      res.status(201).json({ classId: newClass._id });
+      if (existingClass) {
+        res.status(400).json({ message: "Classroom Already Exists" });
+      } else {
+        const classroom = new Classroom({
+          name: req.body.name,
+          grade: existingGrade._id,
+        });
+        const newClass = await classroom.save();
+        res.status(201).json({ classroomId: newClass._id });
+      }
+    } else {
+      res.status(404).json({ message: "Grade Not Found" });
     }
   } catch (err) {
     res.status(400).json({ message: err.message });
